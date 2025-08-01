73c3e64fb1dbb71b1e70723bad507437aa377e3b
rana danish <ranadanish.se@gmail.com>
2025-08-01 20:00:53 +0500

    socket

--- server/socket.js
+++ server/socket.js
@@ -5,7 +5,7 @@ import Channel from './models/ChannelModel.js';
 const setupSocket = (server) => {
   const io = new SocketIOServer(server, {
     cors: {
-      origin: process.env.ORIGIN || 'http://localhost:5173',
+      origin: process.env.ORIGIN  || 'https://yapp-chat-three.vercel.app',
       methods: ['GET', 'POST'],
       credentials: true,
     },
